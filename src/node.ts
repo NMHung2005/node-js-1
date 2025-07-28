b81cd7d4c29acbdc5c528f4c54220e90f55a2479
Nguyen Manh Hung <h388810@gmail.com>
2025-07-28 16:04:45 +0700

    set up view engine

--- src/node.ts
+++ src/node.ts
@@ -1,11 +1,16 @@
 //const express = require("express");
 import express from "express";
 import 'dotenv/config';
+
 const PORT = process.env.PORT || 8080;
 const app = express();
 
+//config view engine
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
 app.get("/", (req, res) => {
-    res.send("Hello World! 123");
+    res.render('home')
 })
 
 app.get("/abc", (req, res) => {
